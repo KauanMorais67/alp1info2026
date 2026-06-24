245688ce82196e01ab7ade2271f8c99c76b7c63f
Kauã Morais <kauamorais3355@gmail.com>
2026-06-24 19:08:57 -0300

    Exercício sobre informar a idade e habilitação para dirigir.

---
+++ Idadepdirigir.py
@@ -0,0 +1,8 @@
+idade = int(input("Digite a sua idade: "))
+habilitacao = input("Você tem habilitação?: ")
+
+if idade >= 18 and habilitacao == "sim":
+    print("Pode dirigir")
+else:
+    print("Não pode dirigir")
+  
